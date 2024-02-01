1d1508e5fe66732b4f1b23f681164771399e9a9f
mgamboa <gamboa.martin@gmail.com>
2024-02-01 17:34:20 -0600

    14.2.0 Se integran elementos de instalacion en not

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -85,6 +85,48 @@ class instalacion
 
     }
 
+    private function not_mensaje_etapa(PDO $link): array|stdClass
+    {
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al create', data:  $create);
+        }
+
+        $foraneas = array();
+        $foraneas['not_mensaje_id'] = new stdClass();
+        $foraneas['pr_etapa_proceso_id'] = new stdClass();
+
+
+        $foraneas_r = $init->foraneas(foraneas: $foraneas,table:  __FUNCTION__);
+
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $foraneas_r);
+        }
+
+
+        $campos = new stdClass();
+
+        $campos->fecha = new stdClass();
+        $campos->fecha->tipo_dato = 'DATE';
+        $campos->fecha->default = '1900-01-01';
+
+
+        $campos_r = $init->add_columns(campos: $campos,table:  __FUNCTION__);
+
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $campos_r);
+        }
+
+        $result = new stdClass();
+        $result->foranenas = $foraneas_r;
+        $result->campos = $campos_r;
+
+        return $result;
+
+    }
+
     private function not_receptor(PDO $link): array|stdClass
     {
         $result = new stdClass();
@@ -205,6 +247,13 @@ class instalacion
         $not_mensaje = $this->not_mensaje(link: $link);
         if(errores::$error){
             return (new errores())->error(mensaje: 'Error al ajustar not_mensaje', data:  $not_mensaje);
+        }
+
+        $result->not_mensaje = $not_mensaje;
+
+        $not_mensaje_etapa = $this->not_mensaje_etapa(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar not_mensaje_etapa', data:  $not_mensaje_etapa);
         }
 
         $result->not_mensaje = $not_mensaje;
